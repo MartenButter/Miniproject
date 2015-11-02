cbd69e1b9172c9b15ea227f64ed327cc5d13016f
Ashwin Bakker <Ashwin.bakker_NL@outlook.com>
2015-11-02 18:42:15 +0100

    dB connection up

--- task_sphere.py
+++ task_sphere.py
@@ -1,6 +1,10 @@
+import sqlite3
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+
+conn = sqlite3.connect('C:/Users/Ashwin/SkyDrive/HU/Programming - TICT-V1PROG-15/Miniproject Programming/task_database.db')
+cur = conn.cursor()
 
 Builder.load_file('task_sphere.kv')
 
@@ -8,9 +12,13 @@ class TaskSphere(Widget):
     pass
 
 class SphereApp(App):
+    cur.execute('SELECT name FROM Tasks')
+    name = cur.fetchone()
+    name = str(name[0])
     def build(self):
         return TaskSphere()
 
 if __name__ =='__main__':
     SphereApp().run()
 
+conn.close()
