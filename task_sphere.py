0b475810722906ec47c902d62da615b657afa044
Ashwin Bakker <Ashwin.bakker_NL@outlook.com>
2015-11-02 16:05:49 +0100

    Begin Spheres

---
+++ task_sphere.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+Builder.load_file('task_sphere.kv')
+
+class TaskSphere(Widget):
+    pass
+
+class SphereApp(App):
+    def build(self):
+        return TaskSphere()
+
+if __name__ =='__main__':
+    SphereApp().run()
