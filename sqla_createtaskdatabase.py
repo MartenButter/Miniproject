4ad5bf960f8a500d236b688da339f187668deb3b
Marten <martenbutter@hotmail.com>
2015-11-04 14:40:27 +0100

    Data extraction methods

--- sqla_createtaskdatabase.py
+++ sqla_createtaskdatabase.py
@@ -1,8 +1,15 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Date
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+
+engine = create_engine('sqlite:///foo.db')
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
 
 class Task(Base):
     __tablename__ = 'Tasks'
@@ -16,6 +23,54 @@ class Task(Base):
     responsible_person_id = Column(Integer, ForeignKey('Persons.id'))
     executer_person_id = Column(Integer, ForeignKey('Persons.id'))
     deadline = Column(DateTime)
+
+    #Gets all the Id's of all parent tasks
+    def getAllParentTasksId():
+        lst = []
+        for instance in session.query(Task).\
+                filter(Task.task_id==None):
+            lst.append(instance.id)
+        #Returns them in a list
+        return lst
+
+    #Gets all the Id's of all children tasks of specified parent Id
+    def getAllChildrenTasksId(parent_id):
+        lst = []
+        for instance in session.query(Task).\
+                filter(Task.task_id==parent_id):
+            lst.append(instance.id)
+        #Returns a list
+        return lst
+
+    #Gets an Task object with specified Id
+    def getTask(id):
+        for instance in session.query(Task).\
+                filter(Task.id==id):
+            #Returns the Task object
+            return instance
+
+    #Gets all Task objects
+    def getAllParentTasksAndId():
+        lst = Task.getAllParentTasksId()
+        task_lst = []
+        for i in lst:
+            task_lst.Append(Task.getTask(i))
+        #Returns a dictionary with Id's as key and Task objects as value
+        return dict(zip(lst,task_lst))
+
+    #Gets all children Task objects of specified Id
+    def getAllChildrenTasksAndId(parent_id):
+        lst = Task.getAllChildrenTasksId(parent_id)
+        task_lst =[]
+        for i in lst:
+            task_lst.append(Task.getTask(i))
+        #Returns a dictionary with Id's as key and Task objects as value
+        return dict(zip(lst,task_lst))
+
+    #Updates an Task object in the database
+    def updateTask(self):
+        session.add(self)
+        session.commit()
 
 class Person(Base):
     __tablename__ = 'Persons'
--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-import sqlite3
-import class_task
+import sqla_createtaskdatabase
 
-conn = sqlite3.connect('task_database.db')
-cur = conn.cursor()
-
-taak = class_task.task(1, "testtaak", "False","een test taak","","een dag","nog niet klaar",2,1,"morgen")
-
-print(taak.new_task_to_tuple())
-
-cur.execute('''INSERT INTO Tasks VALUES(NULL,?,?,?,?,?,?,?,?,?)''', taak.new_task_to_tuple())
-
-conn.commit()
-conn.close()
-
-
-
-
-
-
+obj = sqla_createtaskdatabase.Task.getTask(1)
+print (sqla_createtaskdatabase.Task.taskToList(obj))
